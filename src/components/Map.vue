674c8a302f15cd480bc19fec90630652a4bf6cdd
zrcccrz <zrcccrz@163.com>
2020-07-26 14:31:59 +0800

    Map

---
+++ src/components/Map.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="map-id">
+  </div>
+</template>
+
+<script>
+import L from 'leaflet';
+
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+
+const token = 'pk.eyJ1IjoienJjY2NyeiIsImEiOiJjank2bjgzMzcwZmJqM2RsYjBtbjh2Z3F6In0.Bi-7OaOTBaJ43pJM-hAT-g';
+const style = 'zrcccrz/ckd1yvaxs13an1imn3552xkss';
+export default {
+  name: 'Map',
+
+  data() {
+    return {
+
+    };
+  },
+
+  mounted() {
+    const map = L.map('map-id').setView([30.260574, 120.125254], 15);
+
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, '
+        + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
+      + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      // id: 'mapbox/light-v10',
+      id: style,
+      tileSize: 512,
+      zoomOffset: -1,
+      en_name: 'zh',
+    }).addTo(map);
+  },
+
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss">
+
+#map-id {
+ height: calc(100vh - 104px);
+};
+
+</style>
--- src/views/warning/Warning.vue
+++ src/views/warning/Warning.vue
@@ -1,16 +1,40 @@
-<template v-slot:page-main>
-  <div>hhhhh</div>
+<template>
+  <div class="map-container">
+    <div class="cards-container">
+      <v-card v-for="(d,index) in info" :key="index"
+        outlined
+        width="13vw"
+      >
+        <span class="badge-icon">
+          <component :is= "d.icon"></component>
+        </span>
+        <span>{{d.title}}</span>
+        <span class="number">{{nums[index]}}</span>
+      </v-card>
+    </div>
+    <Map/>
+  </div>
 </template>
 
 <script>
 import layout from '@/mixins/layout';
+import Map from '@/components/Map.vue';
+import Icon1 from '@/assets/warning/icon_1.svg';
+import Icon2 from '@/assets/warning/icon_2.svg';
+import Icon3 from '@/assets/warning/icon_3.svg';
 
 export default {
   name: 'Warning',
   components: {
+    Map,
   },
   data: () => ({
-
+    info: [
+      { icon: Icon1, title: '异常机构数量' },
+      { icon: Icon2, title: '高危机构数量' },
+      { icon: Icon3, title: '正常机构数量' },
+    ],
+    nums: [77, 155, 72],
   }),
 
   mixins: [layout],
@@ -18,6 +42,42 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+  .map-container {
+    width: 77vw;
+    margin: 4px;
+    background: #fff;
+  }
 
+  .cards-container {
+    position: absolute;
+    top: 120px;
+    right: 8%;
+    z-index: 1100;
+    width: 60%;
+    display: flex;
+    justify-content: space-around;
+
+    .v-card {
+      background: rgba(255,255,255,.8)!important;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      font-size: rem;
+
+      .number {
+        margin-right: 1rem;
+      }
+    }
+
+    .badge-icon {
+      display: flex;
+      align-items: center;
+
+      svg {
+        transform: scale(0.7);
+      }
+    }
+
+  }
 </style>
